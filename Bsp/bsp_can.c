ad0bcd76bfdc5845a6f71ba7f7bac4f8bae129cb
Barzello <reuzaki47@gmail.com>
2020-06-24 02:00:21 +0300

    first commit

---
+++ Bsp/bsp_can.c
@@ -0,0 +1,194 @@
+/****************************************************************************
+ *  Copyright (C) 2018 RoboMaster.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+ 
+#include "bsp_can.h"
+#include "bsp_led.h"
+
+moto_info_t motor_info[MOTOR_MAX_NUM];
+uint16_t can_cnt;
+uint32_t motor_id = 4;
+/**
+  * @brief  init can filter, start can, enable can rx interrupt
+  * @param  hcan pointer to a CAN_HandleTypeDef structure that contains
+  *         the configuration information for the specified CAN.
+  * @retval None
+  */
+void can_user_init(CAN_HandleTypeDef* hcan )
+{
+  CAN_FilterTypeDef  can_filter;
+
+  can_filter.FilterBank = 0;                       // filter 0
+  can_filter.FilterMode =  CAN_FILTERMODE_IDMASK;  // mask mode
+  can_filter.FilterScale = CAN_FILTERSCALE_32BIT;
+  can_filter.FilterIdHigh = 0;
+  can_filter.FilterIdLow  = 0;
+  can_filter.FilterMaskIdHigh = 0;
+  can_filter.FilterMaskIdLow  = 0;                // set mask 0 to receive all can id
+  can_filter.FilterFIFOAssignment = CAN_RX_FIFO0; // assign to fifo0
+  can_filter.FilterActivation = ENABLE;           // enable can filter
+  can_filter.SlaveStartFilterBank  = 14;          // only meaningful in dual can mode
+   
+  HAL_CAN_ConfigFilter(hcan, &can_filter);        // init can filter
+  HAL_CAN_Start(&hcan1);                          // start can1
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING); // enable can1 rx interrupt
+}
+
+/**
+  * @brief  can rx callback, get motor feedback info
+  * @param  hcan pointer to a CAN_HandleTypeDef structure that contains
+  *         the configuration information for the specified CAN.
+  * @retval None
+  */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  CAN_RxHeaderTypeDef rx_header;
+  uint8_t             rx_data[8];
+  if(hcan->Instance == CAN1)
+  {
+    HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data); //receive can data
+  }
+  if ((rx_header.StdId >= FEEDBACK_ID_BASE)
+   && (rx_header.StdId <  FEEDBACK_ID_BASE + MOTOR_MAX_NUM))                  // judge the can id
+  {
+    can_cnt ++;
+    uint8_t index = rx_header.StdId - FEEDBACK_ID_BASE;                  // get motor index by can_id
+    motor_info[index].rotor_angle    = ((rx_data[0] << 8) | rx_data[1]);
+    motor_info[index].rotor_speed    = ((rx_data[2] << 8) | rx_data[3]);
+    motor_info[index].torque_current = ((rx_data[4] << 8) | rx_data[5]);
+    motor_info[index].temp           =   rx_data[6];
+  }
+  if (can_cnt == 500)
+  {
+    can_cnt = 0;
+    LED_GREEN_TOGGLE(); // green led blink indicate can comunication successful 
+  }
+}
+
+/**
+  * @brief  send motor control message through can bus
+  * @param  id_range to select can control id 0x1ff or 0x2ff
+  * @param  motor voltage 1,2,3,4 or 5,6,7
+  * @retval None
+  */
+void set_motor_speed_mode()
+{
+	//Send 00 DA 00 19 00 00 00 2F, Return 00 DB 00 19 00 00 00 2F.
+  CAN_TxHeaderTypeDef tx_header;
+  uint8_t             tx_data[8];
+    
+  tx_header.StdId = motor_id;
+  tx_header.IDE   = CAN_ID_STD;
+  tx_header.RTR   = CAN_RTR_DATA;
+  tx_header.DLC   = 8;
+
+  tx_data[0] = 0x00;
+  tx_data[1] = 0xDA;
+  tx_data[2] = 0x00;
+  tx_data[3] = 0x19;
+  tx_data[4] = 0x00;
+  tx_data[5] = 0x00;
+  tx_data[6] = 0x00;
+  tx_data[7] = 0x2F;
+  HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0); 
+}
+void set_motor_acceleration()
+{
+	//Send 00 DA 00 13 00 00 0A 0B, the acceleration time is 10*100ms，and the deceleration time is 11*100ms, Return 00 DB 00 13 00 00 0A 0B.
+	CAN_TxHeaderTypeDef tx_header;
+	uint8_t             tx_data[8];
+
+	tx_header.StdId = motor_id;
+	tx_header.IDE   = CAN_ID_STD;
+	tx_header.RTR   = CAN_RTR_DATA;
+	tx_header.DLC   = 8;
+
+	tx_data[0] = 0x00;
+	tx_data[1] = 0xDA;
+	tx_data[2] = 0x00;
+	tx_data[3] = 0x13;
+	tx_data[4] = 0x00;
+	tx_data[5] = 0x00;
+	tx_data[6] = 0x0A;
+	tx_data[7] = 0x0B;
+	HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0);
+}
+void set_motor_speed()
+{
+	//Send 00 DA 00 11 00 00 05 55, Return 00 DB 00 11 00 00 05 55.
+	CAN_TxHeaderTypeDef tx_header;
+	uint8_t             tx_data[8];
+
+	tx_header.StdId = motor_id;
+	tx_header.IDE   = CAN_ID_STD;
+	tx_header.RTR   = CAN_RTR_DATA;
+	tx_header.DLC   = 8;
+
+	tx_data[0] = 0x00;
+	tx_data[1] = 0xDA;
+	tx_data[2] = 0x00;
+	tx_data[3] = 0x11;
+	tx_data[4] = 0x00;
+	tx_data[5] = 0x00;
+	tx_data[6] = 0x05;
+	tx_data[7] = 0x55;
+	HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0);
+
+}
+void set_motor_enable()
+{
+	//Send 00 DA 00 10 00 00 00 1F. Return 00 DB 00 10 00 00 00 1F.
+	CAN_TxHeaderTypeDef tx_header;
+	uint8_t             tx_data[8];
+
+	tx_header.StdId = motor_id;
+	tx_header.IDE   = CAN_ID_STD;
+	tx_header.RTR   = CAN_RTR_DATA;
+	tx_header.DLC   = 8;
+
+	tx_data[0] = 0x00;
+	tx_data[1] = 0xDA;
+	tx_data[2] = 0x00;
+	tx_data[3] = 0x10;
+	tx_data[4] = 0x00;
+	tx_data[5] = 0x00;
+	tx_data[6] = 0x00;
+	tx_data[7] = 0x1F;
+	HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0);
+
+}
+void set_motor_stop()
+{
+	//Send 00 DA 00 10 00 00 00 0F, the motor stops immediately and the motor is offline. Receive 00 DB 00 10 00 00 00 0F
+	CAN_TxHeaderTypeDef tx_header;
+	uint8_t             tx_data[8];
+
+	tx_header.StdId = motor_id;
+	tx_header.IDE   = CAN_ID_STD;
+	tx_header.RTR   = CAN_RTR_DATA;
+	tx_header.DLC   = 8;
+
+	tx_data[0] = 0x00;
+	tx_data[1] = 0xDA;
+	tx_data[2] = 0x00;
+	tx_data[3] = 0x10;
+	tx_data[4] = 0x00;
+	tx_data[5] = 0x00;
+	tx_data[6] = 0x00;
+	tx_data[7] = 0x0F;
+	HAL_CAN_AddTxMessage(&hcan1, &tx_header, tx_data,(uint32_t*)CAN_TX_MAILBOX0);
+
+}
